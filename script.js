f735219405d06205c1494cea4668a71490656e6f
saaviasole <swapnaliasole@yahoo.com>
2025-12-30 14:20:22 +0530

    space

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
 document.getElementById("firstparagraph").innerHTML="hello friends"
 function myfunction() {
-    var a=4;
-    document.getElementById("buttoncontent").innerHTML=a*a
+    var a=12;
+        b=1
+    document.getElementById("buttoncontent").innerHTML=a*b
 }
 
 
     function validateForm() {
     const email = document.getElementById('email').value;
-    const pass = document.getElementById('Password').value;
+    const Pass = document.getElementById('Password').value;
     const age = document.getElementById('age').value;
-    const address = document.getElementById('address').value;
+    const address = document.getElementById('Address').value;
     const msgBox = document.getElementById('message');
 
     let message = '';
@@ -17,13 +18,17 @@ function myfunction() {
     if (email === '') {
         message = 'Enter an email.';
     msgBox.style.color = 'red';
-      } else if (pass === '') {
+      } else if (Pass === '') {
         message = 'Enter a Password.';
     msgBox.style.color = 'red';
       } else if (age === '') {
         message = 'Enter your age.';
     msgBox.style.color = 'red';
-      } else {
+        } else if (Address === '') {
+         message = 'Enter your age.';
+        msgBox.style.color = 'red';
+    
+         } else {
         message = 'Login successful!';
     msgBox.style.color = 'green';
       }
